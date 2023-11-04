65885a23d30076f78bf43a6d94f50e6d30e6ea37
hemalvarambhia <h.n.varambhia@gmail.com>
2023-11-04 19:37:42 +0000

    Added a test list describing how many players can play trivia.

---
+++ ruby/spec/trivia_game_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+
+describe "Playing Trivia" do
+  describe 'How many players can play trivia' do
+    example 'having 1 player in the game makes it unplayable'
+    example 'up to 6 players can play the game'
+    example 'the game can be played by more than 6 players'
+  end
+end
