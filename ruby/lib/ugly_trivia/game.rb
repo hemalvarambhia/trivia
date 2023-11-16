002fec832c8b6d3fa19a67a80b1f9fcf8a7955d8
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2023-11-16 13:43:22 +0000

    Refactor/Extract Method - moved some code to an intention-revealing method.

--- ruby/lib/ugly_trivia/game.rb
+++ ruby/lib/ugly_trivia/game.rb
@@ -98,7 +98,12 @@ module UglyTrivia
       }[@places[@current_player]]
     end
 
-  public
+    def next_players_turn
+      @current_player += 1
+      @current_player = 0 if @current_player == @players.length
+    end
+
+    public
 
     def was_correctly_answered
       if @in_penalty_box[@current_player]
@@ -127,8 +132,7 @@ module UglyTrivia
         puts "#{@players[@current_player]} now has #{@purses[@current_player]} Gold Coins."
 
         winner = did_player_win
-        @current_player += 1
-        @current_player = 0 if @current_player == @players.length
+        next_players_turn
 
         return winner
       end
@@ -139,8 +143,7 @@ module UglyTrivia
   		puts "#{@players[@current_player]} was sent to the penalty box"
   		@in_penalty_box[@current_player] = true
 
-      @current_player += 1
-      @current_player = 0 if @current_player == @players.length
+      next_players_turn
   		return true
     end
 
