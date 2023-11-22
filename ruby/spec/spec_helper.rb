718e583fc88e2242cf9621c5c3beb525c071e1b6
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2023-11-22 10:36:27 +0000

    Added the kind of coverage we want.

--- ruby/spec/spec_helper.rb
+++ ruby/spec/spec_helper.rb
@@ -5,7 +5,10 @@
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 require 'simplecov'
-SimpleCov.start
+SimpleCov.start do
+  enable_coverage :branch
+  enable_coverage :line
+end
 RSpec.configure do |config|
   config.treat_symbols_as_metadata_keys_with_true_values = true
   config.run_all_when_everything_filtered = true
