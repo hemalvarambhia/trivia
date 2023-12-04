39be0ba2bb95c659351868ecf45dcc83cea41bd0
hemalvarambhia <h.n.varambhia@gmail.com>
2023-12-04 21:15:30 +0000

    refactor: removed obsolete code.

--- ruby/lib/ugly_trivia/game.rb
+++ ruby/lib/ugly_trivia/game.rb
@@ -138,7 +138,7 @@ module UglyTrivia
         3 => 'Rock',
         7 => 'Rock',
         11 => 'Rock'
-      }[place || @places[@current_player]]
+      }[place]
     end
 
     def prepare_questions
