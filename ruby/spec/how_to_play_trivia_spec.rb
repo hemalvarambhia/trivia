7aca7adc5c8047cfa1f9dd9762461900ccce85e7
hemalvarambhia <h.n.varambhia@gmail.com>
2023-12-18 19:07:19 +0000

    test: added a test from our list.

--- ruby/spec/how_to_play_trivia_spec.rb
+++ ruby/spec/how_to_play_trivia_spec.rb
@@ -52,7 +52,12 @@ describe "How to play Trivia" do
     end
 
     context 'Trivia game involving three players' do
-      example 'all players start at the beginning'
+      example 'all players start at the beginning' do
+        game = silent_game_involving(['Player 1', 'Player 2', 'Player 3'])
+
+        expect(game.places).to all(eq(0))
+      end
+
       example 'all players start with no coins in their purse'
       example 'all players do not start in the penalty'
 
