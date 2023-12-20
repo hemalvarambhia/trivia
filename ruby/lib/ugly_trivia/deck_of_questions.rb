48d6463a290a4f903cc9fab123c4e3e1b77fa50e
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2023-12-20 11:47:58 +0000

    format: cleaned up code formatting.

--- ruby/lib/ugly_trivia/deck_of_questions.rb
+++ ruby/lib/ugly_trivia/deck_of_questions.rb
@@ -3,18 +3,10 @@ module UglyTrivia
 
     def initialize
       @categories_per_place = {
-        0 => 'Pop',
-        4 => 'Pop',
-        8 => 'Pop',
-        1 => 'Science',
-        5 => 'Science',
-        9 => 'Science',
-        2 => 'Sports',
-        6 => 'Sports',
-        10 => 'Sports',
-        3 => 'Rock',
-        7 => 'Rock',
-        11 => 'Rock'
+        0 => 'Pop', 4 => 'Pop', 8 => 'Pop',
+        1 => 'Science', 5 => 'Science', 9 => 'Science',
+        2 => 'Sports', 6 => 'Sports', 10 => 'Sports',
+        3 => 'Rock', 7 => 'Rock', 11 => 'Rock'
       }
 
       @questions = {
