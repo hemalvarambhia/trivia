74e677943001020864cfee8b1b254f383c3407db
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2023-12-21 19:06:10 +0000

    fix: wrote the wrong test.

--- ruby/spec/how_to_play_trivia_spec.rb
+++ ruby/spec/how_to_play_trivia_spec.rb
@@ -88,14 +88,15 @@ describe "How to play Trivia" do
   describe 'Rolling the die' do
     it 'does not advance a player when they roll a 0'
 
-    it 'reports who is the current player' do
+    it 'reports who is the current player'
+
+    it 'reports the number the current player rolled' do
       game = game_with_commentary_involving(['Player 1', 'Player 2'])
 
       game.roll(4)
 
       expect(game.commentary).to include('They have rolled a 4')
     end
-    it 'reports the number the current player rolled'
 
     it 'advances a player a number of places as shown on the die' do
       game = silent_game_involving(['Player 1', 'Player 2'])
