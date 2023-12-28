40965884d139ad876ce45413ce584d2f4f29454c
hemalvarambhia <h.n.varambhia@gmail.com>
2023-12-28 20:00:09 +0000

    refactor: use Ruby's Forwardable to delegate methods to the Commentary object.

--- ruby/spec/game_with_commentary.rb
+++ ruby/spec/game_with_commentary.rb
@@ -1,15 +1,10 @@
+require 'forwardable'
 require 'game_commentary'
 class UglyTrivia::GameWithCommentary < UglyTrivia::Game
+  extend Forwardable
+  def_delegators :@commentary, :puts, :commentary
   def initialize
     super
     @commentary = UglyTrivia::GameCommentary.new
-  end
-
-  def commentary
-    @commentary.commentary
-  end
-
-  def puts(message)
-    @commentary.puts(message)
   end
 end
