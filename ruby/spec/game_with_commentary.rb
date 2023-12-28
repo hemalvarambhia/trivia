114bcc2527e45cd1b0461c7f18f0110193d21174
hemalvarambhia <h.n.varambhia@gmail.com>
2023-12-28 20:03:28 +0000

    refactor: where we design the class we wish we had, where we can pass in a Commentary object.

--- ruby/spec/game_with_commentary.rb
+++ ruby/spec/game_with_commentary.rb
@@ -3,8 +3,8 @@ require 'game_commentary'
 class UglyTrivia::GameWithCommentary < UglyTrivia::Game
   extend Forwardable
   def_delegators :@commentary, :puts, :commentary
-  def initialize
-    super
-    @commentary = UglyTrivia::GameCommentary.new
+  def initialize(commentary: UglyTrivia::GameCommentary.new)
+    @commentary = commentary
+    super()
   end
 end
