95734423c0dc0dbca33b66784d9b6615ad60c7c0
hemalvarambhia <h.n.varambhia@gmail.com>
2023-12-28 20:16:59 +0000

    refactor: Introduced a NoGameCommentary class that is polymorphic to GameCommentary and now we can Make Similar Code Even More Similar.

--- ruby/spec/game_with_no_commentary.rb
+++ ruby/spec/game_with_no_commentary.rb
@@ -1,5 +1,10 @@
+require 'forwardable'
+require 'no_game_commentary'
 class UglyTrivia::GameWithNoCommentary < UglyTrivia::Game
-  def puts(message)
-    # do nothing
+  extend Forwardable
+  def_delegators :@commentary, :puts, :commentary
+  def initialize
+    super()
+    @commentary = UglyTrivia::NoGameCommentary.new
   end
 end
---
+++ ruby/spec/no_game_commentary.rb
@@ -0,0 +1,11 @@
+module UglyTrivia
+  class NoGameCommentary
+    def commentary
+      []
+    end
+
+    def puts(message)
+      # no op
+    end
+  end
+end
