b015f2ed1c9b7e92f04eae6a658961b4c12aee7b
hemalvarambhia <h.n.varambhia@gmail.com>
2023-12-29 21:28:06 +0000

    refactor: Move Method where we move the methods to a more sensible place.

--- ruby/spec/string_io_based_game_commentator.rb
+++ ruby/spec/string_io_based_game_commentator.rb
@@ -8,6 +8,26 @@ module UglyTrivia
       @commentary.string.split("\n")
     end
 
+    def commentate_answer_was_correct
+      display "Answer was correct!!!!"
+    end
+
+    def commentate_question_incorrectly_answered
+      display 'Question was incorrectly answered'
+    end
+
+    def commentate_sent_to_penalty_box(trivia_player)
+      display "#{trivia_player.name} was sent to the penalty box"
+    end
+
+    def commentate_gold_coins_won_by(trivia_player)
+      display "#{trivia_player.name} now has #{trivia_player.gold_coins} Gold Coins."
+    end
+
+    def display(message)
+      puts message
+    end
+
     def puts(message)
       @commentary.puts(message)
     end
