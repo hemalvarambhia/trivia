753a314690d10e712dc2a053f41f7b256a0c2f2a
hemalvarambhia <h.n.varambhia@gmail.com>
2023-12-31 23:46:31 +0000

    Moved the golden master related files to their own directory.

--- ruby/run-golden-master.sh
+++ ruby/run-golden-master.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-bin/trivia_for_testing > spec/test-run-2024.txt
-diff spec/test-run-2024.txt spec/golden-master.txt
+bin/trivia_for_testing > spec/runs/test-run-2024.txt
+diff spec/runs/test-run-2024.txt spec/runs/golden-master.txt
