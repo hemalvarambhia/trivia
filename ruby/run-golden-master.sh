7b906838d45debcc84e249b4f27ec423bddb4438
hemalvarambhia <h.n.varambhia@gmail.com>
2024-01-01 11:14:23 +0000

    style: Use double quotes as suggested by the IDE.

--- ruby/run-golden-master.sh
+++ ruby/run-golden-master.sh
@@ -7,6 +7,6 @@ seeds=(7777 1111 9876)
 
 for seed in "${seeds[@]}"
 do
-  bin/trivia_for_testing $seed > spec/runs/test-run-$seed.txt
-  diff spec/runs/test-run-$seed.txt spec/runs/golden-master-$seed.txt
+  bin/trivia_for_testing "$seed" > spec/runs/test-run-"$seed".txt
+  diff spec/runs/test-run-"$seed".txt spec/runs/golden-master-"$seed".txt
 done
