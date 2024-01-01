1d12afa892a4ff0585655e0fbc87056165af9ff4
hemalvarambhia <h.n.varambhia@gmail.com>
2024-01-01 09:40:33 +0000

    refactor: renamed the golden-master file to mention the seed it was generated from.

--- ruby/run-golden-master.sh
+++ ruby/run-golden-master.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 seed=7777
 bin/trivia_for_testing $seed > spec/runs/test-run-$seed.txt
-diff spec/runs/test-run-$seed.txt spec/runs/golden-master.txt
+diff spec/runs/test-run-$seed.txt spec/runs/golden-master-$seed.txt
 
 seeds=(7777)
 
 for seed in "${seeds[@]}"
 do
   bin/trivia_for_testing $seed > spec/runs/test-run-$seed.txt
-  diff spec/runs/test-run-$seed.txt spec/runs/golden-master.txt
+  diff spec/runs/test-run-$seed.txt spec/runs/golden-master-$seed.txt
 done
