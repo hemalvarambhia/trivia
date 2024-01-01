4ce8773a4e2ab0219506e7901d1f778fcf964b9b
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2024-01-01 09:19:07 +0000

    Corrected the seed we use.

--- ruby/run-golden-master.sh
+++ ruby/run-golden-master.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-bin/trivia_for_testing > spec/runs/test-run-2024.txt
-diff spec/runs/test-run-2024.txt spec/runs/golden-master.txt
+bin/trivia_for_testing > spec/runs/test-run-7777.txt
+diff spec/runs/test-run-7777.txt spec/runs/golden-master.txt
