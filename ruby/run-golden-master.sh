9e2a0d817f04eae2a5146d48e2bf61532f469316
hemalvarambhia <h.n.varambhia@gmail.com>
2024-01-01 11:13:19 +0000

    we can now reference multiple golden master

--- ruby/run-golden-master.sh
+++ ruby/run-golden-master.sh
@@ -3,7 +3,7 @@ seed=7777
 bin/trivia_for_testing $seed > spec/runs/test-run-$seed.txt
 diff spec/runs/test-run-$seed.txt spec/runs/golden-master-$seed.txt
 
-seeds=(7777)
+seeds=(7777 1111 9876)
 
 for seed in "${seeds[@]}"
 do
