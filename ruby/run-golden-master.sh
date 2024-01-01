66372ca1b143c94b0cd511e1663424864f86eac6
hemalvarambhia <h.n.varambhia@gmail.com>
2024-01-01 09:39:26 +0000

    refactor: we can iterate over 1 seed.

--- ruby/run-golden-master.sh
+++ ruby/run-golden-master.sh
@@ -2,3 +2,11 @@
 seed=7777
 bin/trivia_for_testing $seed > spec/runs/test-run-$seed.txt
 diff spec/runs/test-run-$seed.txt spec/runs/golden-master.txt
+
+seeds=(7777)
+
+for seed in "${seeds[@]}"
+do
+  bin/trivia_for_testing $seed > spec/runs/test-run-$seed.txt
+  diff spec/runs/test-run-$seed.txt spec/runs/golden-master.txt
+done
