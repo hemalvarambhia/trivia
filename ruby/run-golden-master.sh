1baae6849a5be276a58d8bcd4975255628f7fa2f
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2024-01-01 09:19:53 +0000

    refactor: the seed can be set in the command line.

--- ruby/run-golden-master.sh
+++ ruby/run-golden-master.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-bin/trivia_for_testing > spec/runs/test-run-7777.txt
+bin/trivia_for_testing 7777 > spec/runs/test-run-7777.txt
 diff spec/runs/test-run-7777.txt spec/runs/golden-master.txt
