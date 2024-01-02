48d9da01c9c72e6dcab03c8106d6cc3a0dd9bb7a
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2024-01-02 11:09:02 +0000

    refactor: copied the games to a more sensible place.

---
+++ ruby/spec/testable_games/string_io_based_game_commentator.rb
@@ -0,0 +1,39 @@
+module UglyTrivia
+  class StringIOBasedGameCommentator
+    def initialize
+      @commentary = StringIO.new
+    end
+
+    def commentary
+      @commentary.string.split("\n")
+    end
+
+    def moved(trivia_player)
+      display "#{trivia_player.name}'s new location is #{trivia_player.location}"
+    end
+
+    def answer_was_correct
+      display "Answer was correct!!!!"
+    end
+
+    def question_answered_incorrectly
+      display 'Question was incorrectly answered'
+    end
+
+    def commentate_sent_to_penalty_box(trivia_player)
+      display "#{trivia_player.name} was sent to the penalty box"
+    end
+
+    def commentate_gold_coins_won_by(trivia_player)
+      display "#{trivia_player.name} now has #{trivia_player.gold_coins} Gold Coins."
+    end
+
+    def display(message)
+      puts message
+    end
+
+    def puts(message)
+      @commentary.puts(message)
+    end
+  end
+end
