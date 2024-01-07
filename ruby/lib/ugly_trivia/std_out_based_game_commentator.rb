2fe54345933d5a9000a6ae3cace4fc6f6ab8e359
hemalvarambhia <h.n.varambhia@gmail.com>
2024-01-07 22:34:00 +0000

    removed code that duplicates logic from the mixin.

--- ruby/lib/ugly_trivia/std_out_based_game_commentator.rb
+++ ruby/lib/ugly_trivia/std_out_based_game_commentator.rb
@@ -3,36 +3,6 @@ module UglyTrivia
   class StdOutBasedGameCommentator
     include GameEventListener
 
-    def player_added(trivial_player, number)
-      display "#{trivial_player.name} was added"
-      display "They are player number #{number}"
-    end
-
-    def die_rolled(face_landed_on, trivia_player)
-      display "#{trivia_player.name} is the current player"
-      display "They have rolled a #{face_landed_on}"
-    end
-
-    def moved(trivia_player)
-      display "#{trivia_player.name}'s new location is #{trivia_player.location}"
-    end
-
-    def answer_was_correct
-      display "Answer was correct!!!!"
-    end
-
-    def question_answered_incorrectly
-      display 'Question was incorrectly answered'
-    end
-
-    def sent_to_penalty_box(trivia_player)
-      display "#{trivia_player.name} was sent to the penalty box"
-    end
-
-    def gold_coin_awarded_to(trivia_player)
-      display "#{trivia_player.name} now has #{trivia_player.gold_coins} Gold Coins."
-    end
-
     def display(message)
       puts message
     end
