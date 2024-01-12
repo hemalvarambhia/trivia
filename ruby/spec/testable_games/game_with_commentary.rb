d780602c10068793eea02fcd6ba6385673adcd44
hemalvarambhia <h.n.varambhia@gmail.com>
2024-01-12 23:26:35 +0000

    refactor: removed obsolete code.

--- ruby/spec/testable_games/game_with_commentary.rb
+++ ruby/spec/testable_games/game_with_commentary.rb
@@ -3,14 +3,7 @@ require 'string_io_based_game_commentator'
 class UglyTrivia::GameWithCommentary < UglyTrivia::Game
   extend Forwardable
   def_delegators :@commentary,
-                 :puts,
                  :commentary,
-                 :player_added,
-                 :die_rolled,
-                 :moved,
-                 :answer_was_correct,
-                 :question_answered_incorrectly,
-                 :commentate_sent_to_penalty_box,
                  :gold_coin_awarded_to
   def initialize(commentary: UglyTrivia::StringIOBasedGameCommentator.new)
     super
