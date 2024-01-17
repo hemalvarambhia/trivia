f65360d63bd82b505154cb8ebcd58d616e802079
hemalvarambhia <h.n.varambhia@gmail.com>
2024-01-17 21:01:33 +0000

    refactor: pushed a detail up the callstack.

--- ruby/lib/ugly_trivia/deck_of_questions.rb
+++ ruby/lib/ugly_trivia/deck_of_questions.rb
@@ -1,7 +1,12 @@
 module UglyTrivia
   class DeckOfQuestions
 
-    def initialize
+    def initialize(questions_by_category = {
+          'Pop' => Array.new(50) { |i| "Pop Question #{i}" },
+          'Science' => Array.new(50) { |i| "Science Question #{i}" },
+          'Sports' => Array.new(50) { |i| "Sports Question #{i}" },
+          'Rock' => Array.new(50) { |i| "Rock Question #{i}" }
+        })
       @categories_per_place = {
         0 => 'Pop', 4 => 'Pop', 8 => 'Pop',
         1 => 'Science', 5 => 'Science', 9 => 'Science',
@@ -9,12 +14,7 @@ module UglyTrivia
         3 => 'Rock', 7 => 'Rock', 11 => 'Rock'
       }
 
-      @questions = {
-        'Pop' => Array.new(50) { |i| "Pop Question #{i}" },
-        'Science' => Array.new(50) { |i| "Science Question #{i}" },
-        'Sports' => Array.new(50) { |i| "Sports Question #{i}" },
-        'Rock' => Array.new(50) { |i| "Rock Question #{i}" }
-      }
+      @questions = questions_by_category
     end
 
     def pick_question_for(category)
