db42ef6ca91991e94f3c1e7a0a563221352e338b
Hemal Varambhia <h.n.varambhia@gmail.com>
2024-02-18 15:42:50 +0000

    refactor: structured the tests by grouping them into the context that applies.

--- ruby/spec/playing_trivia_spec.rb
+++ ruby/spec/playing_trivia_spec.rb
@@ -12,12 +12,14 @@ describe 'How to play Trivia' do
     specify 'Reports the number the current player rolled'
 
     specify 'Moves the current player nowhere when a zero is rolled'
-    specify 'Moves the current player a number of places as shown on the face of the die'
-    specify 'Does not move any other player from their current place'
-    specify 'Rolling a negative number moves the current player backwards by that number'
 
-    specify "Reports the current player's new location"
-    specify "Reports the category of question that will be asked to the current player"
-    specify "Asks the current player a question from the category for their current location"
+    context 'Given the player has rolled an odd number' do
+      specify 'Moves the current player a number of places as shown on the face of the die'
+      specify 'Does not move any other player from their current place'
+      specify 'Rolling a negative number moves the current player backwards by that number'
+      specify "Reports the current player's new location"
+      specify "Reports the category of question that will be asked to the current player"
+      specify "Asks the current player a question from the category for their current location"
+    end
   end
 end
