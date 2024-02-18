549e6677fe33f532d39558363906e770dea38528
Hemal Varambhia <h.n.varambhia@gmail.com>
2024-02-18 14:57:34 +0000

    test: we can now compare 401 Golden Masters masters to optimise for safety.

--- ruby/run-golden-master.sh
+++ ruby/run-golden-master.sh
@@ -1,9 +1,16 @@
 #!/bin/sh
 
-seeds=(7777 1111 9876 12345 654321)
+seeds=()
 
-for seed in "${seeds[@]}"
+# Generate 100 random seeds and store them in the array
+for ((i=0; i<=500; i++)); do
+    number=$((7777 + (i * 100)))
+    seeds+=($number)
+done
+
+for ((i=0; i<=500; i++));
 do
+  seed=$((7777 + (i * 100)))
   bin/trivia_for_testing "$seed" > spec/runs/test-run-"$seed".txt
   diff spec/runs/test-run-"$seed".txt spec/runs/golden-master-"$seed".txt
 done
