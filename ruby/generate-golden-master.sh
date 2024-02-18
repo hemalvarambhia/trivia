909293af8d7d6a3375dae6aece2316b195916c66
Hemal Varambhia <h.n.varambhia@gmail.com>
2024-02-18 12:52:24 +0000

    test: we can generate more golden masters deterministically.

--- ruby/generate-golden-master.sh
+++ ruby/generate-golden-master.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
-seeds=(7777 1111 9876 12345 654321)
+seeds=()
+
+# Generate 100 random seeds and store them in the array
+for ((i=0; i<=400; i++)); do
+    number=$((7777 + (i * 100)))
+    seeds+=($number)
+done
 
 for seed in "${seeds[@]}"
 do
