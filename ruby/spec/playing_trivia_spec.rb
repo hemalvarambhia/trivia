8bd309aa51d226a9d05c3d26362d9c66e6099597
Hemal Varambhia <h.n.varambhia@gmail.com>
2024-02-18 15:40:43 +0000

    test: added more tests to the list.

--- ruby/spec/playing_trivia_spec.rb
+++ ruby/spec/playing_trivia_spec.rb
@@ -8,9 +8,16 @@ describe 'How to play Trivia' do
   end
 
   describe 'Rolling the dice' do
+    specify 'Reports who the current player is'
+    specify 'Reports the number the current player rolled'
+
     specify 'Moves the current player nowhere when a zero is rolled'
     specify 'Moves the current player a number of places as shown on the face of the die'
     specify 'Does not move any other player from their current place'
     specify 'Rolling a negative number moves the current player backwards by that number'
+
+    specify "Reports the current player's new location"
+    specify "Reports the category of question that will be asked to the current player"
+    specify "Asks the current player a question from the category for their current location"
   end
 end
