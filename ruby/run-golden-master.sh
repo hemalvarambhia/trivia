a317e57b99fc940a0a1bd011dc78af8aa3118838
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2024-02-20 15:45:41 +0000

    refactor: removed obsolete code.

--- ruby/run-golden-master.sh
+++ ruby/run-golden-master.sh
@@ -1,13 +1,5 @@
 #!/bin/sh
 
-seeds=()
-
-# Generate 100 random seeds and store them in the array
-for ((i=0; i<=500; i++)); do
-    number=$((7777 + (i * 100)))
-    seeds+=($number)
-done
-
 for ((i=0; i<=500; i++));
 do
   seed=$((7777 + (i * 100)))
