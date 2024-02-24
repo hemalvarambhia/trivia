4b18d58e9b9688054631e28fe38af68f81774760
Hemal Varambhia <h.n.varambhia@gmail.com>
2024-02-24 15:31:03 +0000

    test: we can now demonstrate that a game with no players is unplayable.

--- python/number_of_players_test.py
+++ python/number_of_players_test.py
@@ -1,9 +1,11 @@
 import unittest
+from trivia import Game
 
 
 class NumberOfPlayersTest(unittest.TestCase):
     def test_that_a_trivia_game_with_no_players_is_unplayable(self):
-        self.assertEqual(4, 2 + 2)
+        game = Game()
+        self.assertEqual(False, game.is_playable())
 
 
 if __name__ == '__main__':
