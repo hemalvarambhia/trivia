e9fcd0ac2583c0e7423dc0f1d69cfae193a32ca3
Hemal Varambhia <h.n.varambhia@gmail.com>
2024-02-24 21:20:18 +0000

    test: added another assertion to the test.

--- python/number_of_players_test.py
+++ python/number_of_players_test.py
@@ -6,6 +6,7 @@ class NumberOfPlayersTest(unittest.TestCase):
     def test_that_a_trivia_game_with_no_players_is_unplayable(self):
         game = Game()
         self.assertEqual(False, game.is_playable())
+        self.assertEqual(0, game.how_many_players)
 
 
 if __name__ == '__main__':
