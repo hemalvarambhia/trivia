82c4d89ca70c2dafbaf9ce09f6d2f192ffab1e88
Hemal Varambhia <h.n.varambhia@gmail.com>
2024-02-25 15:32:27 +0000

    refactor: Extract Method to house some duplicate code

--- python/test/number_of_players_test.py
+++ python/test/number_of_players_test.py
@@ -24,14 +24,17 @@ class NumberOfPlayersTest(unittest.TestCase):
         self.assertEqual(2, game.how_many_players)
 
     def test_that_a_trivia_game_with_more_than_two_players_is_playable(self):
-        game = Game()
-        game.add('Player 1')
-        game.add('Player 2')
-        game.add('Player 3')
-        game.add('Player 4')
+        game = self.__game_with(['Player 1', 'Player 2', 'Player 3', 'Player 4'])
 
         self.assertEqual(True, game.is_playable())
         self.assertEqual(4, game.how_many_players)
+
+    def __game_with(self, players):
+        game = Game()
+        for player in players:
+            game.add(player)
+
+        return game
 
 
 if __name__ == '__main__':
