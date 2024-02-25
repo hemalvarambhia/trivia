594f42f2e4c54d527094211302f6850f4390600f
Hemal Varambhia <h.n.varambhia@gmail.com>
2024-02-25 19:01:00 +0000

    test: added a test to our list to describe how rolling the die works.

---
+++ python/test/test_rolling_the_die.py
@@ -0,0 +1,11 @@
+import unittest
+
+
+class TestRollingTheDie(unittest.TestCase):
+    @unittest.skip('Not implemented yet')
+    def test_that_rolling_die_reports_who_the_current_player_is(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
