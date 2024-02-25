50423e2b1700cd1eb7d8feac5c97abf92dac60f4
Hemal Varambhia <h.n.varambhia@gmail.com>
2024-02-25 19:19:53 +0000

    test: added a test where we use a testable subclass.

--- python/test/test_rolling_the_die.py
+++ python/test/test_rolling_the_die.py
@@ -1,10 +1,23 @@
 import unittest
 
+from trivia import Game
+import sys
+
+class GameWithCommentary(Game):
+    pass
+
 
 class TestRollingTheDie(unittest.TestCase):
-    @unittest.skip('Not implemented yet')
+
+    @unittest.skip("Not implemented")
     def test_that_rolling_die_reports_who_the_current_player_is(self):
-        pass
+        game = GameWithCommentary()
+        game.add('Player 1')
+        game.add('Player 2')
+
+        game.roll(3)
+
+        self.assertEqual('Player 1 is the current player', game.commentary)
 
 
 if __name__ == '__main__':
