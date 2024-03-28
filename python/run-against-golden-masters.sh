6fb6de161bcad1cb5a280417acc8f10fdfd5c68a
Hemal Varambhia <h.n.varambhia@gmail.com>
2024-03-28 11:54:26 +0000

    shortened the paths to make reading the script simpler.

--- python/run-against-golden-masters.sh
+++ python/run-against-golden-masters.sh
@@ -1,7 +1,8 @@
+
 for num in $(seq 0 799)
 do
   gameId=$((7777 + num * 15))
-  python python/play_trivia_for_testing.py --seed $gameId > python/test/runs/test-run-$gameId.txt
-  diff python/test/runs/test-run-$gameId.txt python/test/golden_masters/golden-master-$gameId.txt
-  rm python/test/runs/test-run-$gameId.txt
+  python play_trivia_for_testing.py --seed $gameId > test/runs/test-run-$gameId.txt
+  diff test/runs/test-run-$gameId.txt test/golden_masters/golden-master-$gameId.txt
+  rm test/runs/test-run-$gameId.txt
 done
