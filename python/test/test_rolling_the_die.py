cf02a32ae19982e26e163692f158d89d7dd0e4b0
Hemal Varambhia <h.n.varambhia@gmail.com>
2024-03-28 12:39:17 +0000

    test/refactor: changed the test as category and question seem related.

--- python/test/test_rolling_the_die.py
+++ python/test/test_rolling_the_die.py
@@ -32,7 +32,7 @@ class TestRollingTheDie(unittest.TestCase):
 
             self.assertIn("Player 1's new location is 6", game.commentary())
 
-    def test_game_reports_the_category_of_question_that_will_be_asked_at_their_location(self):
+    def test_game_asks_the_category_of_question_for_their_location_on_the_board(self):
         with GameWithCommentary() as game:
             game.add('Player 1')
             game.add('Player 2')
@@ -40,6 +40,7 @@ class TestRollingTheDie(unittest.TestCase):
             game.roll(6)
 
             self.assertIn("The category is Sports", game.commentary())
+            self.assertIn("Sports Question 0", game.commentary())
 
 if __name__ == '__main__':
     unittest.main()
