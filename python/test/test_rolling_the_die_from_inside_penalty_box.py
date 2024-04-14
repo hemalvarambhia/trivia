5a345a49718a16f1678756b7b588932e28c34b86
Hemal Varambhia <h.n.varambhia@gmail.com>
2024-04-14 23:02:23 +0100

    refactor: what if instead, we get the item of interest rather than check inclusion.

--- python/test/test_rolling_the_die_from_inside_penalty_box.py
+++ python/test/test_rolling_the_die_from_inside_penalty_box.py
@@ -25,7 +25,7 @@ class TestRollingTheDieFromInsidePenaltyBox(unittest.TestCase):
             # Current Player rolls an odd number.
             game.roll(1)
 
-            self.assertIn('Current Player is getting out of the penalty box', game.commentary())
+            self.assertEqual('Current Player is getting out of the penalty box', game.commentary()[-4])
 
 
     def test_current_player_moves_a_number_of_steps_forward_when_they_roll_an_odd_number(self):
@@ -45,7 +45,7 @@ class TestRollingTheDieFromInsidePenaltyBox(unittest.TestCase):
            # Current Player rolls an odd number.
            game.roll(1)
 
-           self.assertIn("Current Player's new location is 6", game.commentary())
+           self.assertEqual("Current Player's new location is 6", game.commentary()[-3])
 
 
 if __name__ == '__main__':
