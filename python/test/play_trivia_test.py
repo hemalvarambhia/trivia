fe4ce98936d539d1c4d89efc62dcbf017c2dc974
Hemal Varambhia <h.n.varambhia@gmail.com>
2024-04-20 11:46:22 +0100

    refactor: added another argument to the method so we can migrate clients to it and remove the old arguments.

--- python/test/play_trivia_test.py
+++ python/test/play_trivia_test.py
@@ -2,9 +2,13 @@ import unittest
 
 
 class PlayTriviaTest(unittest.TestCase):
-    def between(self, game, player1, player2):
-        game.add(player1)
-        game.add(player2)
+    def between(self, game, player1 = None, player2 = None, players = []):
+        if len(players) == 0:
+            game.add(player1)
+            game.add(player2)
+        else:
+            for player in players:
+                game.add(player)
 
 
 if __name__ == '__main__':
