7107ff5d1b393035789e0fd46e21ca7820d31f85
Hemal Varambhia <h.n.varambhia@gmail.com>
2024-04-20 11:29:29 +0100

    refactor: minimised duplication with custom assertion. Changed ordering of assertions to show how one assertion implies the other.

--- python/test/test_number_of_players.py
+++ python/test/test_number_of_players.py
@@ -22,37 +22,38 @@ class SilentGame(Game):
 class TestNumberOfPlayers(unittest.TestCase):
     def test_that_a_trivia_game_with_no_players_is_unplayable(self):
         game = Game()
+
+        self.__assertHasPlayers(0, game)
         self.assertEqual(False, game.is_playable())
-        self.assertEqual(0, game.how_many_players)
 
     def test_that_a_trivia_game_with_one_player_is_unplayable(self):
         with SilentGame() as game:
             game.add('Player 1')
 
+            self.__assertHasPlayers(1, game)
             self.assertEqual(False, game.is_playable())
-            self.assertEqual(1, game.how_many_players)
 
     def test_that_a_trivia_game_with_two_players_is_playable(self):
         with SilentGame() as game:
             game.add('Player 1')
             game.add('Player 2')
 
-            self.assertEqual(True, game.is_playable())
-            self.assertEqual(2, game.how_many_players)
+            self.__assertHasPlayers(2, game)
+            self.__assertIsPlayable(game)
 
     def test_that_a_trivia_game_with_more_than_two_players_is_playable(self):
         game = (
             self.__game_with(['Player 1', 'Player 2', 'Player 3', 'Player 4']))
 
-        self.assertEqual(True, game.is_playable())
-        self.assertEqual(4, game.how_many_players)
+        self.__assertHasPlayers(4, game)
+        self.__assertIsPlayable(game)
 
     def test_that_a_trivia_game_with_five_players_is_playable(self):
         game = (
             self.__game_with(['Player 1', 'Player 2', 'Player 3', 'Player 4', 'Player 5']))
 
-        self.assertEqual(True, game.is_playable())
-        self.assertEqual(5, game.how_many_players)
+        self.__assertHasPlayers(5, game)
+        self.__assertIsPlayable(game)
 
     def test_that_six_players_cannot_play_trivia(self):
         self.assertRaises(IndexError, lambda: self.__game_with(
@@ -68,6 +69,12 @@ class TestNumberOfPlayers(unittest.TestCase):
                 game.add(player)
 
         return game
+
+    def __assertIsPlayable(self, game):
+        self.assertEqual(True, game.is_playable())
+
+    def __assertHasPlayers(self, expected, game):
+        self.assertEqual(expected, game.how_many_players)
 
 
 if __name__ == '__main__':
