f194c2ab640e0256236849a81ff4ffb8890f355d
Hemal Varambhia <h.n.varambhia@gmail.com>
2024-04-21 22:11:33 +0100

    refactor: migrated tests to using the between method.

--- python/test/test_number_of_players.py
+++ python/test/test_number_of_players.py
@@ -4,6 +4,11 @@ import sys
 
 
 class SilentGame(Game):
+
+    def between(self, players):
+        for player in players:
+            self.add(player)
+
     def __init__(self):
         self._stdout = sys.stdout
         super().__init__()
@@ -28,25 +33,24 @@ class TestNumberOfPlayers(unittest.TestCase):
 
     def test_that_a_trivia_game_with_one_player_is_unplayable(self):
         with SilentGame() as game:
-            game.add('Player 1')
+            game.between(['Player 1'])
 
             self.__assertHasPlayers(1, game)
             self.__assertNotPlayable(game)
 
     def test_that_a_trivia_game_with_two_players_is_playable(self):
         with SilentGame() as game:
-            game.add('Player 1')
-            game.add('Player 2')
+            game.between(['Player 1', 'Player 2'])
 
             self.__assertHasPlayers(2, game)
             self.__assertIsPlayable(game)
 
     def test_that_a_trivia_game_with_more_than_two_players_is_playable(self):
-        game = (
-            self.__game_with(['Player 1', 'Player 2', 'Player 3', 'Player 4']))
+        with SilentGame() as game:
+            game.between(['Player 1', 'Player 2', 'Player 3', 'Player 4'])
 
-        self.__assertHasPlayers(4, game)
-        self.__assertIsPlayable(game)
+            self.__assertHasPlayers(4, game)
+            self.__assertIsPlayable(game)
 
     def test_that_a_trivia_game_with_five_players_is_playable(self):
         game = (
