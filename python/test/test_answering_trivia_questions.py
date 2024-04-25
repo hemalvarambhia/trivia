d3c8b314b54b7c35c9a1ed69e590026f96980b99
Hemal Varambhia <h.n.varambhia@gmail.com>
2024-04-25 14:04:38 +0100

    test: added a test from the list.

--- python/test/test_answering_trivia_questions.py
+++ python/test/test_answering_trivia_questions.py
@@ -1,9 +1,21 @@
+import io
 import unittest
+from contextlib import redirect_stdout
+
+from test.game_with_commentary import GameWithCommentary
+
 
 class TestAnsweringTriviaQuestions(unittest.TestCase):
-    @unittest.skip('Test list')
+
     def test_answering_trivia_question_correctly_wins_current_player_a_gold_coin(self):
-        pass
+        with (io.StringIO() as fake_out, redirect_stdout(fake_out)):
+            game = GameWithCommentary()
+            game.between(['Player 1', 'Irrelevant'])
+            game.roll(2)
+
+            game.was_correctly_answered()
+
+            self.assertEqual(1, game.purses[0])
 
     @unittest.skip('Test list')
     def test_answering_trivia_question_correctly_keeps_current_player_out_of_penalty_box(self):
