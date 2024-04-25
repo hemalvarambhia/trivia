9fc1bf835ea93f08eb8d41ad97c8d3bbdca57ae2
Hemal Varambhia <h.n.varambhia@gmail.com>
2024-04-25 13:15:18 +0100

    test: added another test to the list.

--- python/test/test_answering_trivia_questions.py
+++ python/test/test_answering_trivia_questions.py
@@ -1,13 +1,12 @@
 import unittest
-from ugly_trivia.trivia import Game
-from contextlib import redirect_stdout
-import io
-from test.game_with_commentary import GameWithCommentary
-
 
 class TestAnsweringTriviaQuestions(unittest.TestCase):
     @unittest.skip('Test list')
     def test_answering_trivia_question_correctly_wins_current_player_a_gold_coin(self):
+        pass
+
+    @unittest.skip('Test list')
+    def test_answering_trivia_question_correctly_keeps_current_player_out_of_penalty_box(self):
         pass
 
     @unittest.skip('Test list')
