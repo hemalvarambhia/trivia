6364e52a8735931cce0228822a0bfee41d23e5a5
Hemal Varambhia <h.n.varambhia@gmail.com>
2024-05-12 22:00:41 +0100

    refactor: used a def statement to make the test easier to understand.

--- python/test/test_answering_trivia_questions.py
+++ python/test/test_answering_trivia_questions.py
@@ -37,13 +37,14 @@ class TestAnsweringTriviaQuestions(unittest.TestCase):
         pass
 
     def test_answering_trivia_question_incorrectly_does_not_award_player_a_gold_coin(self):
+        def purse(trivia_game): return getattr(trivia_game, "purses")
         with (io.StringIO() as fake_out, redirect_stdout(fake_out)):
             game = GameWithCommentary()
             game.between(['Player 1', 'Irrelevant'])
             game.roll(2)
 
             self.assertChanges(
-                code_block=lambda: game.wrong_answer(), property=lambda: getattr(game, "purses")[0], by=0
+                code_block=lambda: game.wrong_answer(), property=lambda: purse(game)[0], by=0
             )
 
     def assertChanges(self, property, by, code_block):
