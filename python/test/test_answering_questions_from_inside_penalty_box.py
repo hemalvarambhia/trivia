5ed794f6b84f750f041cecc22ec0f734308d6d49
Hemal Varambhia <h.n.varambhia@gmail.com>
2024-05-19 22:08:03 +0100

    test: added a test list for answering the questions from inside a penalty box.

---
+++ python/test/test_answering_questions_from_inside_penalty_box.py
@@ -0,0 +1,19 @@
+import unittest
+
+
+class TestAnsweringQuestionsFromInsidePenaltyBox(unittest.TestCase):
+    @unittest.skip("TODO")
+    def test_answering_question_correctly_allows_current_player_to_leave_penalty_box(self):
+        pass
+
+    @unittest.skip("TODO")
+    def test_answering_question_correctly_wins_current_player_a_gold_coin(self):
+        pass
+
+    @unittest.skip("TODO")
+    def test_answering_question_incorrectly_keeps_current_player_in_penalty_box(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
