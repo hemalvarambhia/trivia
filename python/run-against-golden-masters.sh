e722054460459140347ae62642cfa520a270854e
Hemal Varambhia <h.n.varambhia@gmail.com>
2025-03-02 10:11:37 +0000

    chore: use pipenv to run the code against golden masters.

--- python/run-against-golden-masters.sh
+++ python/run-against-golden-masters.sh
@@ -2,7 +2,7 @@
 for num in $(seq 0 799)
 do
   gameId=$((7777 + num * 15))
-  python play_trivia_for_testing.py --seed $gameId > test/runs/test-run-$gameId.txt
+  pipenv run python play_trivia_for_testing.py --seed $gameId > test/runs/test-run-$gameId.txt
   diff test/runs/test-run-$gameId.txt test/golden_masters/golden-master-$gameId.txt
   rm test/runs/test-run-$gameId.txt
 done
